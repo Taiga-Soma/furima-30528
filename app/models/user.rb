4ef16e45d292d8b22d5dcab6cd2a2da0ead55fc1
Taiga-Soma <allvictory111@gmail.com>
2020-10-17 16:14:34 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   validates :nickname, presence: true, length: { maximum: 40 }
   validates :firstname, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :lastname, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
