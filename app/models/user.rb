532f6a8ae549987043a28057b8ccd9f7f0dc72d7
Taiga-Soma <allvictory111@gmail.com>
2020-10-26 11:16:06 +0900

    passwordのvalidation追加実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   has_many :address
   has_many :items
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
   with_options presence: true do
     validates :nickname, length: { maximum: 40 }
     validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
@@ -13,5 +15,9 @@ class User < ApplicationRecord
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday
+    /\A[a-z0-9]+\z/
+    
+    validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+   
   end
 end
